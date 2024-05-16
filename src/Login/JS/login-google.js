86e3da6b366d277491cb1be6b854c4546fc74ee9
Eduarda Vieira <159597766+eduardavieira-dev@users.noreply.github.com>
2024-05-16 16:33:49 -0300

    login com google

---
+++ src/Login/JS/login-google.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('#login-google').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'PHP/callback-log.php',
+            type: 'GET',
+            success: function(response) {
+                var data = JSON.parse(response);
+                window.location.href = data.redirectUrl;
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        });
+
+    });
+});
