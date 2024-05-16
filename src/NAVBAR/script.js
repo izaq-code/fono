223870918a8436dbf0315d71d43534465994b724
cout"izaq <122700689+izaq-code@users.noreply.github.com>
2024-05-16 01:06:09 -0300

    navbar

---
+++ src/NAVBAR/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    function toggleSubMenu(submenuId, iconId, newIconClass) {
+        const submenu = document.getElementById(submenuId);
+        const icon = document.getElementById(iconId);
+
+        submenu.classList.toggle('visible');
+
+
+        if (submenu.classList.contains('visible')) {
+          
+            icon.className = ''; 
+            icon.classList.add('bi', newIconClass); 
+        } else {
+         
+            icon.className = ''; 
+            icon.classList.add('bi', 'bi-caret-up-fill'); 
+        }
+    }
+
+    const menuHeader = document.getElementById('menuHeader');
+    menuHeader.addEventListener('click', function() {
+        toggleSubMenu('menu', 'menuIcon', 'bi-caret-down-fill');
+    });
+
+    const settingsHeader = document.getElementById('settingsHeader');
+    settingsHeader.addEventListener('click', function() {
+        toggleSubMenu('settings', 'settingsIcon', 'bi-caret-down-fill'); 
+    });
+
+    const checkbox = document.getElementById('inputto');
+    checkbox.addEventListener('change', function() {
+        const body = document.body;
+        body.classList.toggle('dark-mode', this.checked);
+    });
+});
