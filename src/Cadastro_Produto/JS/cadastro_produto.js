4390d291c5a2141381d4afb6adb22f2f607fecf6
Porttuga <andre.cardosocp02@gmail.com>
2024-05-24 14:50:51 -0300

    Cadastro Produto atualizado com envio de imagem

--- src/Cadastro_Produto/JS/cadastro_produto.js
+++ src/Cadastro_Produto/JS/cadastro_produto.js
@@ -2,22 +2,34 @@ $(document).ready(function(){
     $('#Cadastro').submit(function(e){
         e.preventDefault();
 
+        var formData = new FormData();
+        var inputName = 'foto_produto';
+
+        var inputFile = document.getElementById('foto_produto');
+
+        if (!inputFile || inputFile.files.length === 0) {
+            console.error("O campo de imagem não foi encontrado ou está vazio.");
+        } else {
+           
+            formData.append(inputName, inputFile.files[0]);
+        }
+
         var nome = $('#nome').val();
-        var preco = $('#preco').val();
+        var preco = $('#preco').val().replace(',' , '.');
         var categoria = $('#categoria').val();
         var descricao = $('#descricao').val();
 
-        var data = {
-            nome: nome,
-            preco: preco,
-            categoria: categoria,
-            descricao: descricao,
-        };
+        formData.append('nome', nome);
+        formData.append('preco', preco);
+        formData.append('categoria', categoria);
+        formData.append('descricao', descricao);
     
         $.ajax({
             type: 'POST',
             url: './PHP/cadastro_produto.php',
-            data: data,
+            data: formData,
+            contentType: false,
+            processData: false,
             success: function(response){
                 console.log(response);
             },
--- src/Cadastro_Produto/PHP/cadastro_produto.php
+++ src/Cadastro_Produto/PHP/cadastro_produto.php
@@ -6,24 +6,26 @@
     $preco = $_POST['preco'];
     $categoria = $_POST['categoria'];
     $descricao = $_POST['descricao'];
+    $diretorio = "../../Imagens_produto/";
 
-//    $inputName = 'foto_produto'; 
-// $nomeArquivo = $diretorio . basename($_FILES[$inputName]['name']);
+    $nomeArquivo = $diretorio . basename($_FILES["foto_produto"]["name"]);
 
-// if (!empty($_FILES[$inputName]['foto_produto'])) { 
-//     if (move_uploaded_file($_FILES[$inputName]['tmp_name'], $nomeArquivo)) {
-//         $nomeArquivos[] = $nomeArquivo;
-//         $caminhosArquivos[] = mysqli_real_escape_string($conexao, $nomeArquivo);
-// }}
+    if (move_uploaded_file($_FILES["foto_produto"]["tmp_name"], $nomeArquivo)) {
+        echo "O arquivo foi enviado com sucesso.";
+    } else {
+        echo "Houve um erro ao enviar o arquivo.";
+    }
+} else {
+    echo "A requisição não foi feita corretamente.";
+}
 
-    $sql = "INSERT INTO cadastro_produto (nome_produto, preco_produto, categoria_produto, 
-                descricao_produto) VALUES ('$nome', '$preco', '$categoria', '$descricao')";
+    $sql = "INSERT INTO cadastro_produto (imagem_do_produto, nome_produto, preco_produto, categoria_produto, 
+                descricao_produto) VALUES ('$nomeArquivo', '$nome', '$preco', '$categoria', '$descricao')";
     
  if ($conexao->query($sql) === TRUE) {
     echo "Produto cadastrado com sucesso !!";
  } else {
     echo "Produto cadastrado não cadastrado !!";
  }
-}
-
+ $conexao->close();
 ?>
