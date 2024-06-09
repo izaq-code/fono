36ad5b9a53e67b546f6d6c19b7f1b0f16521606d
cout"izaq <122700689+izaq-code@users.noreply.github.com>
2024-06-09 17:50:21 -0300

    aa

--- src/relatório/JS/detalhes.js
+++ src/relatório/JS/detalhes.js
@@ -12,14 +12,19 @@ $(document).ready(function() {
             },
             success: function(response) {
                 if (response.status === 'Em análise') {
-                    var detalhesHtml = '<p>Produto: ' + response.nome_produto + '</p>';
+                    var detalhesHtml = '<div class="produto">';
+                    detalhesHtml += '<h3>Produto</h3>';
+                    detalhesHtml += '<p>' + response.nome_produto + '</p>';
                     detalhesHtml += '<p>Preço: ' + response.preco_produto + '</p>';
                     detalhesHtml += '<p>Data da Venda: ' + response.data + '</p>';
-                    detalhesHtml += '<img src="' + response.imagem_produto + '" alt="Imagem do Produto" />';
-                    detalhesHtml += '<p><span style="display: none;"> ' + response.status + '</span></p>';
-                    detalhesHtml += ' <textarea id="descricao" name="message" rows="4" placeholder="Mensagem" required></textarea>';
-                    detalhesHtml += '<button id="salvarDescricao">Enviar</button>';
+                    detalhesHtml += '<div class="img-container"><img src="' + response.imagem_produto + '" alt="Imagem do Produto" /></div>';
+                    detalhesHtml += '<p>Descrição:</p>';
+                    detalhesHtml += '<textarea id="descricao" class="descricao-input" name="message" rows="4" placeholder="Mensagem" required minlength="10000" style="width: 100%; box-sizing: border-box; margin: 10px 0; padding: 10px; font-family: inherit;">' + response.descricao + '</textarea>';
+                    detalhesHtml += '<button id="salvarDescricao" class="detalhes-btn">Enviar</button>';
                     $('#detalhesVenda').html(detalhesHtml);
+                    
+                    
+                    
 
                     $('#salvarDescricao').click(function() {
                         var descricao = $('#descricao').val();
@@ -41,15 +46,25 @@ $(document).ready(function() {
                         });
                     });
                 } else if (response.status === 'Finalizado') {
-                    var detalhesHtml = '<p>Produto: ' + response.nome_produto + '</p>';
+                    var detalhesHtml = '<div class="relatorio">';
+                    detalhesHtml += '<h4>Relatório</h4>';
+                    detalhesHtml += '<div class="descricao">' + response.descricao + '</div>';
+                    detalhesHtml += '</div>';
+                    detalhesHtml += '<div class="produto">';
+                    detalhesHtml += '<h3>Produto</h3>';
+                    detalhesHtml += '<p>' + response.nome_produto + '</p>';
                     detalhesHtml += '<p>Preço: ' + response.preco_produto + '</p>';
                     detalhesHtml += '<p>Data da Venda: ' + response.data + '</p>';
-                    detalhesHtml += '<img src="' + response.imagem_produto + '" alt="Imagem do Produto" />';
-                    detalhesHtml += '<p><span style="display: none;"> ' + response.status + '</span></p>';
-                    detalhesHtml += '<p>Descrição: ' + response.descricao + '</p>';
-                    detalhesHtml += '<input id="descricao" value="' + response.descricao + '"></input>';
-                    detalhesHtml += '<button id="atualizarDescricao">Atualizar Descrição</button>';
+                    detalhesHtml += '<div class="img-container"><img src="' + response.imagem_produto + '" alt="Imagem do Produto" /></div>';
+                    detalhesHtml += '<p>Descrição:</p>';
+                    detalhesHtml += '<textarea id="descricao" class="descricao-input" name="message" rows="4" placeholder="Mensagem" required minlength="10000" style="width: 100%; box-sizing: border-box; margin: 10px 0; padding: 10px; font-family: inherit;">' + response.descricao + '</textarea>';
+                    detalhesHtml += '<button id="atualizarDescricao" class="detalhes-btn">Atualizar Descrição</button>';
+                    detalhesHtml += '</div>';
+                    
                     $('#detalhesVenda').html(detalhesHtml);
+                    
+                    
+                    
 
                     $('#atualizarDescricao').click(function() {
                         var descricao = $('#descricao').val();
